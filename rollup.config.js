817dd7c96e3b792945628827445d35bb3329b750
Elizabeth Byrne <liz.judd@ibm.com>
2020-05-14 10:00:46 -0400

    build: add deprecated files to rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -32,4 +32,30 @@ export default [{
     babel({ babelHelpers: 'bundled' }),
     terser()
   ]
+},
+
+/**
+ * DEPRECIATED FILES
+ * @todo remove these on the next major release
+ */
+{
+  input: 'src/index.js',
+  output: { // ES2015 (ES6) Module
+    file: 'index.js',
+    format: 'es',
+  },
+},
+{
+  // Outputs we want to be transpiled by Babel
+  input: 'src/index.js',
+  output: [
+    { // Common JS
+      file: 'es5.js',
+      format: 'cjs',
+      sourcemap: true,
+    },
+  ],
+  plugins: [
+    babel({ babelHelpers: 'bundled' }),
+  ]
 }];

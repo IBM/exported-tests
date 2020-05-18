04616e4878a13eb0f4db2aca71ad18c8e1e0d97c
Elizabeth Byrne <liz.judd@ibm.com>
2020-05-18 13:59:13 -0400

    refactor: remove tests for inaccurate test structure

--- src/parsers/BDD.ts
+++ src/parsers/BDD.ts
@@ -61,11 +61,6 @@ class BDDTestParser extends TestParser {
    *    handling now be done by TypeScript and we can remove it from our source code?
    */
   suiteSetup(test: TestSuite, fragment: DocumentFragment): void {
-    if (typeof test.before === 'function') {
-      console.warn(
-        'Exported Tests do not support `before` use the `beforeAll` property instead'
-      );
-    }
     const wndw = this.window;
     if (typeof test.beforeAll === 'function') {
       // Mocha uses the before function; Jest uses beforeAll
@@ -90,11 +85,6 @@ class BDDTestParser extends TestParser {
    * @private
    */
   suiteCleanup(test: TestSuite, fragment: DocumentFragment): void {
-    if (typeof test.after === 'function') {
-      console.warn(
-        'Exported Tests do not support `after` use the `afterAll` property instead'
-      );
-    }
     const wndw = this.window;
     if (typeof test.afterAll === 'function') {
       // Mocha uses the after function; Jest uses afterAll

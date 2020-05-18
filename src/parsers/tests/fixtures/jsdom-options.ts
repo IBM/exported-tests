58cf9fc13d2bf3dcc5d89019debe07bda4597b2c
Elizabeth Byrne <liz.judd@ibm.com>
2020-05-18 13:48:00 -0400

    build: update correct JSDom  option type

--- src/parsers/tests/fixtures/jsdom-options.ts
+++ src/parsers/tests/fixtures/jsdom-options.ts
@@ -4,9 +4,10 @@
  * This source code is licensed under the Apache-2.0 license found in the
  * LICENSE file in the root directory of this source tree.
  */
+import { ConstructorOptions } from 'jsdom';
 
 /**
  * Options when creating the window/document from the HTML that's being tested
  * @type {object}
  */
-export default { resources: 'usable' };
+export default ({ resources: 'usable' } as ConstructorOptions);

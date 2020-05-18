a6e04b9c7ff4052b9a691fe4dd79d36a6d1b2847
Elizabeth Byrne <liz.judd@ibm.com>
2020-05-18 17:58:56 -0400

    build: add TypeScript to Rollup build

--- rollup.config.js
+++ rollup.config.js
@@ -2,60 +2,73 @@
 import babel from '@rollup/plugin-babel';
 // Used to minify files
 import { terser } from "rollup-plugin-terser";
+import typescript from '@rollup/plugin-typescript';
+
 import pkg from './package.json';
 
-export default [{
-  input: 'src/index.js',
-  output: { // ES2015 (ES6) Module
-    file: pkg.module,
-    format: 'es',
-    sourcemap: true,
-  },
-  plugins: [terser()]
-},{
-  // Outputs we want to be transpiled by Babel
-  input: 'src/index.js',
-  output: [
-    { // Common JS
-      file: pkg.main,
-      format: 'cjs',
+export default [
+  {
+    input: 'src/index.ts',
+    output: { // ES2015 (ES6) Module
+      file: pkg.module,
+      format: 'es',
       sourcemap: true,
     },
-    { // Browser
-      name: 'ExportedTests',
-      file: pkg.browser,
-      format: 'umd',
-      sourcemap: true,
-    }
-  ],
-  plugins: [
-    babel({ babelHelpers: 'bundled' }),
-    terser()
-  ]
-},
+    plugins: [
+      typescript(),
+      terser()
+    ]
+  },
+  {
+    // Outputs we want to be transpiled by Babel
+    input: 'src/index.ts',
+    output: [
+      { // Common JS
+        file: pkg.main,
+        format: 'cjs',
+        sourcemap: true,
+      },
+      { // Browser
+        name: 'ExportedTests',
+        file: pkg.browser,
+        format: 'umd',
+        sourcemap: true,
+      }
+    ],
+    plugins: [
+      typescript(),
+      babel({ babelHelpers: 'bundled' }),
+      terser()
+    ]
+  },
 
-/**
- * DEPRECIATED FILES
- * @todo remove these on the next major release
- */
-{
-  input: 'src/index.js',
-  output: { // ES2015 (ES6) Module
-    file: 'index.js',
-    format: 'es',
-  },
-},
-{
-  // Outputs we want to be transpiled by Babel
-  input: 'src/index.js',
-  output: [
-    { // Common JS
-      file: 'es5.js',
-      format: 'cjs',
-      sourcemap: true,
+  /**
+   * DEPRECIATED FILES
+   * @todo remove these on the next major release
+   */
+  {
+    input: 'src/index.ts',
+    output: { // ES2015 (ES6) Module
+      file: 'index.js',
+      format: 'es',
     },
-  ],
-  plugins: [
-    babel({ babelHelpers: 'bundled' }),
-  ]
-}];
+    plugins: [
+      typescript(),
+    ]
+  },
+  {
+    // Outputs we want to be transpiled by Babel
+    input: 'src/index.ts',
+    output: [
+      { // Common JS
+        file: 'es5.js',
+        format: 'cjs',
+        sourcemap: true,
+      },
+    ],
+    plugins: [
+      typescript(),
+      babel({ babelHelpers: 'bundled' }),
+    ]
+  }
+];
